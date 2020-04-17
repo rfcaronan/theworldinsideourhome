45bca00f10c5f225348c2229e1ccfcf99726ba2a
Rowena Caronan <caronanr@cardiff.ac.uk>
2020-04-17 01:54:32 +0100

    added about page

--- js/classtoggle.js
+++ js/classtoggle.js
@@ -1,4 +1,4 @@
-// init controller
+// Story pages
 var controller = new ScrollMagic.Controller(
 	{globalSceneOptions: {
 		duration: 800
@@ -19,3 +19,39 @@ new ScrollMagic.Scene({triggerElement: "#part-3"})
 	// Add class toggle
     .setClassToggle("#sec3Button", "active")
     .addTo(controller);
+
+
+// About page
+
+ var controller2 = new ScrollMagic.Controller(
+	{globalSceneOptions: {
+		duration: 300
+	}
+});
+
+
+// build scenes
+new ScrollMagic.Scene({triggerElement: "#part-1-about"})
+	// Add class toggle
+    .setClassToggle("#about1Button", "active-pink")
+    .addTo(controller2);
+new ScrollMagic.Scene({triggerElement: "#part-2-about"})
+	// Add class toggle
+    .setClassToggle("#about2Button", "active-pink") 
+    .addTo(controller2);
+new ScrollMagic.Scene({triggerElement: "#part-3-about"})
+	// Add class toggle
+    .setClassToggle("#about3Button", "active-pink")
+    .addTo(controller2);
+new ScrollMagic.Scene({triggerElement: "#part-4-about"})
+	// Add class toggle
+    .setClassToggle("#about4Button", "active-pink")
+    .addTo(controller2);
+new ScrollMagic.Scene({triggerElement: "#part-5-about"})
+	// Add class toggle
+    .setClassToggle("#about5Button", "active-pink")
+    .addTo(controller2);
+
+
+
+
